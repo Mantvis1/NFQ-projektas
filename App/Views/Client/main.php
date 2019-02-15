065e1b290773f0d7ad24d653f51025c506210a27
Mantvis1 <37077417+Mantvis1@users.noreply.github.com>
2019-02-15 19:20:19 +0200

    Add files via upload

--- App/Views/Client/main.php
+++ App/Views/Client/main.php
@@ -2,16 +2,33 @@
 
 <head>
   <style>
+  .btn-primary {
+    color: #fff;
+    background-color: #2C3E50;
+    border-color: #2C3E50
+  }
+
+  .btn-lg {
+    padding: 0.5rem 1rem;
+    font-size: 1.171875rem;
+    line-height: 1.5;
+    border-radius: 0.3rem
+  }
+
+  .btn:hover {
+    color: #212529;
+    text-decoration: none
+  }
   </style>
 </head>
 
 <body>
   <form action="../../Controllers/clientController.php" method="POST">
-    <label value="">Iveskite savo varda</label>
-    <input type="text" name="name">
-    <label value="">Iveskite savo pavarde</label>
-    <input type="text" name="surname">
-    <button type="submit">Ieskoti</button>
+    <label value="">Iveskite savo varda</label><br>
+    <input type="text" required name="name"><br>
+    <label value="">Iveskite savo pavarde</label><br>
+    <input type="text" required name="surname"><br>
+    <button class="btn btn-primary btn-lg" type="submit">Ieskoti</button>
     <input type="hidden" name="postClientName" value="1" readonly>
   </form>
 </body>
--- App/Views/Haircutter/cancel.php
+++ App/Views/Haircutter/cancel.php
@@ -2,16 +2,31 @@
 
 <head>
   <style>
-  </style>
-  <script>
+  .btn-primary {
+    color: #fff;
+    background-color: #2C3E50;
+    border-color: #2C3E50
+  }
 
-  </script>
+  .btn-lg {
+    padding: 0.5rem 1rem;
+    font-size: 1.171875rem;
+    line-height: 1.5;
+    border-radius: 0.3rem
+  }
+
+  .btn:hover {
+    color: #212529;
+    text-decoration: none
+  }
+  </style>
+
 </head>
 
 <body>
   <form action="../../Controllers/haircutterController.php" method="POST">
     <label>Ivesti kliento varda:</label><input name="customerName">
-    <button type="submit">Paieska</button>
+    <button class="btn btn-primary btn-lg" type="submit">Paieska</button>
     <input name="findInfoAboutOneCustomer" type="hidden" value="1">
   </form>
   <br>
